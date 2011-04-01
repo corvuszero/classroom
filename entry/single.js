ae9aafd534faae7743806ccea044471a2ebbaf59
Marko Alvarez <alvarez@ikigaming.com>
2011-04-01 05:24:56 -0700

    Ehm...no se Far

--- entry/single.js
+++ entry/single.js
@@ -1,14 +1,18 @@
 jsio('import shared.FloorManager as FloorManager');
+jsio('import timestep.Sprite');
+jsio('import timestep.View');
 
 var app = new GCApp();
 var keyListener = app.getKeyListener();
 var mainView = app.getView();
 
-var speed = 3;
-var friction = 2;
 var floorManager;
 
-jsio('import timestep.View');
+var speed = 3;
+var speedX = 0.5;
+var speedY = 0;
+var gravity = 0.5;
+
 var runnerView = new timestep.View
 ({
   x:0,
@@ -27,7 +31,6 @@ var backgroundView = new timestep.View
   parent:runnerView
 });
 
-jsio('import timestep.Sprite');
 var runner = new timestep.Sprite
 ({
   x:20,
@@ -45,8 +48,6 @@ var runner = new timestep.Sprite
       frames:
       [
         [0,0],
-        [64, 0],
-        [128, 0],
         [192, 0]
       ]
     },
@@ -55,15 +56,48 @@ var runner = new timestep.Sprite
       width:64,
       height:108,
       imageURL: 'images/player.png',
-      frameRate:1,
+      frameRate:8,
       frames:
-      [[128, 0]]
+      [
+        [64, 0],
+        [128, 0]
+      ]
+    },
+    jump:
+    {
+        width:64,
+        height:108,
+        imageURL: 'images/player.png',
+        frameRate:8,
+        frames:
+        [
+            [192, 0]
+        ]
     }
   },
   defaultAnimation:'run',
   parent:runnerView,
   zIndex: 1
 });
+
+runner.startAnimation('run');
+runner.isFalling = false;
+runner.isJumping = false;
+
+runner.jump = function()
+{
+    if ( !this.isJumping && !this.isFalling)
+    {
+        this.isJumping = true;
+        this.stopAnimation();
+        this.startAnimation('jump', { iterations: 5, callback: this.jumpFinished } );
+    }
+}
+
+runner.jumpFinished = function()
+{
+    runner.isJumping = false;
+}
 
 backgroundView.render = function(ctx)
 {
@@ -76,10 +110,65 @@ backgroundView.tick = function()
   floorManager.checkFloors();
 }
 
-runner.startAnimation('run');
 floorManager = new FloorManager
 ({
   acceleration:this.acceleration,
   speed:this.speed,
-  platformParent:runnerView,
+  platformParent:runnerView
 });
+
+runnerView.tick = function(dt) 
+{
+	var events = keyListener.popEvents();
+	for (var i = 0; i < events.length; i++) 
+	{
+		var event = events[i];
+		
+		// SHOOTING
+		if (event.code == keyListener.SPACE && event.lifted) 
+		{
+			runner.startAnimation('shoot', { iterations:1 });
+		}
+		// JUMPING
+		else if (event.code == keyListener.UP && event.lifted)
+		{
+		  runner.jump();
+		}
+	}
+};
+
+mainView.tick = function(dt)
+{
+    var platforms = floorManager.getPlatforms();
+    var colliding = false;
+    var jumping   = false;
+   
+    for (var i in platforms)
+    {
+        var floor = platforms[i];
+        if(runner.style.x >= floor.style.x && runner.style.x < (floor.style.x+floor.style.width))
+        {
+            if (runner.style.y + runner.style.height + 5 < floor.style.y)
+            {
+                colliding = false;
+                runner.isFalling = true;
+            }
+            else
+            { 
+                colliding = true;
+                runner.isFalling = false;
+            }
+        }
+    }
+    
+    if ( runner.isJumping )
+    {
+        runner.style.y -= 10;
+    }
+    
+    speedX        += 0.5;
+    speedY        = (colliding) ? 0:(speedY+gravity);
+    runner.style.x += 1;
+    runner.style.y += speedY;
+    
+};
